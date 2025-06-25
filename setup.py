ddfb1ccca68bed13d11cd84f67dab5dd7e9fccb8
srinath <srinath8789@gmail.com>
2025-06-25 17:57:16 +0530

    file updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'srinath',
+    author_email= 'srinath8789@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
